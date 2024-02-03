da7259b2deecb7d52946b8e55254cf09ee819520
Adamvancouver <adamvancouver@gmail.com>
2024-02-03 13:30:13 -0500

    1:30pm

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import {Routes, Route} from 'react-router-dom';
 import Home from "./components/Home";
-//import Journal from "./components/Journal";
+import Journal from "./components/Journal";
 
 export default function App () {
   return (
     <Routes>
       <Route path='/' element={<Home/>}/>
-      {/*<Route path='/journal' element={<Journal/>}/>*/}
+      <Route path='/journal' element={<Journal/>}/>
     </Routes>
 
   );
--- client/src/components/Journal.js
+++ client/src/components/Journal.js
@@ -2,30 +2,16 @@ import React from 'react'
 
 export default function Journal() {
   return (
-    <div>
-        <p className="text-orange-500 ">journal</p>
+    <div style={{backgroundColor: 'orange'}}>
+        <p className="text-orange-500">journal</p>
         <div class="container-fluid">
             <div class = "col-lg-3"></div>
-            <div class="col-lg-6">
-            <center style="background-color: white; width: 100%; border-radius: 20px 20px 20px 20px; padding: 0 50px 100px 50px;">
-                <p style="font-family: Verdana; font-size: 16px; padding: 35px 10px 15px 10px;">Log in with</p> 
-                <div class="g-signin2" data-onsuccess="onSignIn" data-theme="dark" style="width: 250px; height: 50px;"></div>
-                
-                <div class="col-lg-12" style="text-align: left; padding: 0 0 0 0;">
-                    <p style="text-align:left; position:absolute;">Email</p>
-                    <input type="text" id="email" name="email" style="position: relative; left: 0; top: 30px; width: 100%; height: 40px; padding: 0 0 10px 0;">
-                    <p style="padding: 15px 0 0 0;">Password</p>
-                    <input type="text" id="password" name="password" style="position: relative; left: 0; top: 0; width: 100%; height: 40px; padding: 0 0 0 0;">
-                </div>
-                <span style="padding: 60px 0 10px 0;">
-                 <button style="width: 100%; height: 40px; border-width:0; color:white; background-color: rgb(86,187,118); ">LOG IN (coming soon...)</button>
-                </span>
-                <div style="text-align:right; position:absolute; right:65px;">
-                    <a href="https://millbay.waterquality.live/reset.html" style="color: black;">Forgot your password?</a>
-                </div>
-            </center>
-      </div> 
-        <div class="col-lg-3"></div>
+            <div class= "col-lg-6">
+                <center style={{ backgroundColor: 'white', width: '100%', borderRadius: '20px', padding: '0 50px 100px 50px' }}>
+                    hello
+                </center>
+            </div> 
+            <div class="col-lg-3"></div>
         </div>
     </div>
   );
