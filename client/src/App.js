31988d0f4903be9fa82d490917bacf13f2a099db
Adamvancouver <adamvancouver@gmail.com>
2024-02-03 13:02:41 -0500

    Co-authored-by: ms6124 <ms6124@users.noreply.github.com> Co-authored-by: teresashao <teresashao@users.noreply.github.com> Co-authored-by: Keegan-L <Keegan-L@users.noreply.github.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,27 @@
-import React, {useState, useEffect} from 'react' 
+import React from "react";
+import Home from "./components/Home";
+import Journal from "./components/Journal";
 
 function App() {
-  
-  const [data, setData] = useState([{}])
-
-  useEffect(() => {
-    fetch("/member").then(
-      res => res.json()
-    ).then(
-      data => {
-        setData(data)
-        console.log(data)
-      }
-    )
-  }, [])
   return (
-    <div className="App">
-      {(typeof data.members == 'undefined') ? (
-        <p>Loading...</p>
-      ) : (
-        data.members.map((member, i) => (
-          <p key = {i}>{member}</p>
-        ))
-      )}
+    <div>
+      <header class="relative flex items-center justify-center h-screen mb-12 overflow-hidden">
+        {/*<video
+          autoPlay
+          loop
+          muted
+          class="absolute z-10 w-auto min-w-full min-h-full max-w-none"
+        >
+          <source
+            src={videoBg}
+            type="video/mp4"
+          />
+  </video>*/}
+        <div className="relative z-30 p-5 text-2xl text-white h-screen min-w-full min-h-full bg-gray-900 bg-opacity-80 rounded-xl">
+          <Home />
+          <Journal /> 
+        </div>
+      </header>
     </div>
   );
 }
---
+++ client/src/components/Journal.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Journal() {
+  return (
+    <div>
+        <p className="text-orange-500 ">journal</p>
+    </div>
+    
+  );
+}
