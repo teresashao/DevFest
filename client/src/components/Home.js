58b9e9e87ed42665b59697f6ece5f8f144540739
Adamvancouver <adamvancouver@gmail.com>
2024-02-03 15:19:41 -0500

    Co-authored-by: ms6124 <ms6124@users.noreply.github.com> Co-authored-by: Keegan-L <Keegan-L@users.noreply.github.com> Co-authored-by: teresashao <teresashao@users.noreply.github.com>

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Home() {
   return (
@@ -12,18 +13,20 @@ function Home() {
       <div className="flex flex-col z-30 p-5 h-screen min-w-full min-h-full items-center justify-center">
         <img
           src="/blob-light.png"
-          className="absolute z-10 w-7/12 h-auto"
+          className="absolute z-10 w-7/12 "
           alt="Blob Light Image"
         />
         <img
           src="/blob-solid.png"
-          className="absolute z-10 w-6/12 h-auto"
+          className="absolute z-10 w-6/12 "
           alt="Blob Solid Image"
         />
-        <div className="flex flex-col w-5/12 items-center justify-centerz-20">
-          <h1 className="text-8xl font-righteous relative z-20 mt-2"> Journify</h1>
-          <h2 className="text-lg font-mont relative z-20 mt-10 text-center"> Write, Thrive, Unwind - elevate your mood!</h2>
-          <button className="text-3xl font-mont relative z-20 mt-20 text-center bg-slate-100 text-slate-500 rounded-lg py-3 px-7 transform transition-transform duration-300 hover:hover:drop-shadow-xl hover:scale-105">Write</button>
+        <div className="flex flex-col w-1/2 items-center justify-center z-20">
+          <h1 className="text-8xl font-righteous h-1/3 relative z-20 mt-2"> Journify</h1>
+          <h2 className="text-lg font-mont relative w-1/2 z-20 mt-10 text-center"> Write, Thrive, Unwind - elevate your mood!</h2>
+          <Link to="/journal"> {/* Replace '/your-link-path' with your actual path */} 
+            <button className="text-3xl font-mont relative z-20 mt-20 text-center bg-slate-100 text-slate-500 rounded-lg py-3 px-7 transform transition-transform duration-300 hover:hover:drop-shadow-xl hover:scale-105">Write</button>
+          </Link>
         </div>
       </div>
     </header>
--- client/src/components/Journal.js
+++ client/src/components/Journal.js
@@ -1,40 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Journal() {
+  const [journalText, setJournalText] = useState('');
+
+  const handleTextareaClick = () => {
+    // Clear the placeholder text when the user clicks on the textarea
+    if (journalText === 'Start typing here...') {
+      setJournalText('');
+    }
+  }
   return (
+
 
     <div>
       <header className='relative flex items-center justify-center h-screen overflow-hidden'>
-        <img src="https://t4.ftcdn.net/jpg/01/87/47/59/360_F_187475954_KuDIRQZbGTwwyMTXZeRmojQH9YeSjrWt.jpg" className="absolute z-10 w-auto min-w-full min-h-full max-w-none" alt="background"/>
+        <img src="/ocean.jpeg" className="absolute z-10 w-auto min-w-full min-h-full max-w-none" alt="background" />
         <div className="relative z-30 p-5 text-2xl text-white h-screen min-w-full min-h-full items-center justify-center">
           <div className="container-fluid">
             <div className="row">
-              <div className="col-lg-3"></div>
-              <div className="col-lg-6">
+              <div className="col-lg-2"></div>
+              <div className="col-lg-8">
                 <div className="text-center" style={{ backgroundColor: 'rgba(255, 255, 255, 0.8)', borderRadius: '20px', padding: '20px' }}>
-                  <p className='text-blue-500' style={{textAlign: 'left', padding: '30px 0 0 0' }}></p>
+                  <p className='text-blue-500' style={{ textAlign: 'left', padding: '30px 0 0 0' }}></p>
                   <div className="col-lg-12" style={{ textAlign: 'left', padding: '0 0 0 0' }}>
                     {/* <p style={{ textAlign: 'left', position: 'absolute' }}>Input your journal here:</p> */}
                     {/* <input type="text" id="journal" name="journal" style={{ position: 'relative', left: 0, top: '30px', width: '100%', height: '100px', padding: '0 0 10px 0' }} /><br /><br /> */}
-                    <label htmlFor="journal" className="text-5xl block mb-10 text-blue-500 font-righteous" style={{marginTop: '30px', width: '100%' }}>Journal</label>
+                    <label htmlFor="journal" className="text-5xl block mb-10 text-blue-500 font-righteous" style={{ marginTop: '30px', width: '100%' }}>Journal</label>
                     <textarea
                       id="journal"
                       name="journal"
-                      className="w-full h-48 p-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500 text-sm text-black"
+                      value={journalText}
+                      className="font-mont w-full h-80 p-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500 text-sm text-black"
+                      onClick={handleTextareaClick}
+                      onChange={(e) => setJournalText(e.target.value)}
+                      placeholder="Instructions: blablabla"
                     />
-                    <span style={{ padding: '20px 0 10px 0' }}>
+                    <span style={{ padding: '0 0 10px 0' }}>
                       {/* <button style={{ width: '100%', height: '40px', borderWidth: 0, color: 'white', backgroundColor: 'rgb(86, 187, 118)' }}>
                         Submit
                       </button> */}
-                    <center>
-                      <button className="text-3xl font-mont relative z-20 mt-20 text-center bg-slate-100 text-slate-500 rounded-lg py-3 px-7 transform transition-transform duration-300 hover:hover:drop-shadow-xl hover:scale-105">Submit</button>
+                      <center>
+                      <Link to="../"> {/* Replace '/your-link-path' with your actual path */} 
+                        <button className="text-3xl font-mont relative z-20 mt-20 mr-5 text-center bg-slate-100 text-slate-500 rounded-lg py-3 px-7 transform transition-transform duration-300 hover:hover:drop-shadow-xl hover:scale-105">Back</button>
+                      </Link>
+                        <button className="text-3xl font-mont relative z-20 mt-10 ml-5 text-center bg-slate-100 text-slate-500 rounded-lg py-3 px-7 transform transition-transform duration-300 hover:hover:drop-shadow-xl hover:scale-105">Submit</button>
                       </center>
                     </span>
                   </div>
                 </div>
               </div>
-              <div className="col-lg-3"></div>
+              <div className="col-lg-2"></div>
             </div>
           </div>
         </div>
