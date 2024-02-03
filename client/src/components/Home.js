11ee0c7de532bc221204294399ee8834d5c4322a
Adamvancouver <adamvancouver@gmail.com>
2024-02-03 13:14:47 -0500

    1:14pm

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,8 +2,44 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
+    <header class="relative flex items-center justify-center h-screen mb-12 overflow-hidden">
+        <img 
+        src="/ocean.png"
+        class="absolute z-10 w-auto min-w-full min-h-full max-w-none"
+        alt="ocean"
+        />
+        <div className="relative z-30 p-5 text-2xl text-white h-screen min-w-full min-h-full bg-gray-900 bg-opacity-80 rounded-xl">
+            <p>home</p>
+        </div>
+    </header>
   )
 }
 
-export default Home
+export default Home;
+
+/*
+{
+    return (
+      <div>
+        <header class="relative flex items-center justify-center h-screen mb-12 overflow-hidden">
+          <video
+            autoPlay
+            loop
+            muted
+            class="absolute z-10 w-auto min-w-full min-h-full max-w-none"
+          >
+            <source
+              src={videoBg}
+              type="video/mp4"
+            />
+          </video>
+          <div className="relative z-30 p-5 text-2xl text-white h-screen min-w-full min-h-full bg-gray-900 bg-opacity-80 rounded-xl">
+            <Navbar/>
+            <Home />
+          </div>
+        </header>
+      </div>
+    );
+  }
+  
+  export default App; */
