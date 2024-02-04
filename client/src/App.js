86072f9902a2b283c45dc8febf7c43c1f3120600
ms6124 <122077962+ms6124@users.noreply.github.com>
2024-02-04 10:09:28 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,48 @@
-import React, {useState, useEffect} from 'react' 
+import React, { useState, useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from "./pages/Home";
+import Journal from "./pages/Journal";
+import { CSSTransition } from 'react-transition-group';
 
-function App() {
-  
-  const [data, setData] = useState([{}])
 
-  useEffect(() => {
-    fetch("/member").then(
-      res => res.json()
-    ).then(
-      data => {
-        setData(data)
-        console.log(data)
-      }
-    )
-  }, [])
+export default function App() {
+  const [data, setData] = useState("");
+  const [emotion, setEmotion] = useState("");
+
+  const [audioUrl, setAudioUrl] = useState('');
+
+  const handleHome = () => {
+    setData("");
+    setEmotion("");
+  };
+
+
+  function handlePositionData(entry) {
+    console.log('Data received from child:', entry);
+
+    // Make a POST request to your Flask backend
+    fetch("/send-entry", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ text: entry }), 
+    })
+      .then((res) => res.json())
+      .then((response) => {
+        console.log("Response from Flask backend:", response.message);
+        setData(response.message);
+        setEmotion(response.emotion);
+      })
+      .catch((error) => {
+        console.error("Error sending data to Flask backend:", error);
+      });
+  }
+
   return (
-    <div className="App">
-      {(typeof data.members == 'undefined') ? (
-        <p>Loading...</p>
-      ) : (
-        data.members.map((member, i) => (
-          <p key = {i}>{member}</p>
-        ))
-      )}
-    </div>
+    <Routes>
+      <Route path='/' element={<Home handleHome={handleHome}/>} />
+      <Route path='/journal' element={<Journal data={data} emotion={emotion} sendDataToParent={handlePositionData} />} />
+    </Routes>
   );
 }
-
-export default App;
